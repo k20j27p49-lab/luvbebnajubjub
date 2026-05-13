4561523cb2b6d4e7d6cc99ccaa2185ea5e9ac07e
yunokana <yanades.dis@gmail.com>
2026-05-13 18:26:48 +0700

    Enhance task scheduling functionality in main.c
    Refactored task management logic and added scheduling features.

--- main.c
+++ main.c
@@ -1,60 +1,72 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include "scheduler.h"
 
-struct node {
-    char name[50];
-    int duration;
-    int deadline;
-    int priority;
-    int dependency;
-    struct node*next;
-    struct node*prev;
-};
+int main() {
 
-struct node* createNode(struct node Task) {
-    struct node* newNode = malloc(sizeof(struct node));
-    *newNode = Task;
-    newNode->next = NULL;
-    newNode->prev = NULL;
-    return newNode;
-}
+    int choice;
 
-void getinfo(struct node** tasks) {
-    int n;
-    printf("Enter number of tasks: ");
-    scanf("%d", &n);
+    while(1) {
 
-    for (int i = 0; i < n; i++) {
-        struct node temp;
+        printf("\n===== SMART SCHEDULER =====\n");
 
-        printf("Enter name of task %d: ", i + 1);
-        scanf("%s", temp.name);
+        printf("1. Add Task\n");
+        printf("2. Generate Schedule\n");
+        printf("3. Display Tasks\n");
+        printf("4. Exit\n");
 
-        printf("Enter duration of task %d (in hours): ", i + 1);
-        scanf("%d", &temp.duration);
+        printf("Choose: ");
+        scanf("%d", &choice);
 
-        printf("Enter deadline of task %d (in hours): ", i + 1);
-        scanf("%d", &temp.deadline);
+        if(choice == 1) {
 
-        printf("Enter priority of task %d (1-10): ", i + 1);
-        scanf("%d", &temp.priority);
+            addTaskFile();
+        }
 
-        printf("Enter dependency of task %d (index or -1): ", i + 1);
-        scanf("%d", &temp.dependency);
+        else if(choice == 2) {
 
-        struct node* newNode = createNode(temp);
+            loadTasks();
 
-        if (*tasks == NULL) {
-            *tasks = newNode;
-        } else {
-            struct node* current = *tasks;
-            while (current->next != NULL) {
-                current = current->next;
+            createGraph();
+
+            if(detectCycle()) {
+
+                printf("\nCycle Detected!\n");
+                printf("Invalid dependency graph.\n");
+
+                continue;
             }
-            current->next = newNode;
-            newNode->prev = current;
+
+            topologicalSort();
+
+            sortByPriority();
+
+            printTopo();
+
+            scheduleTasks();
+
+            saveSchedule();
+
+            printf("\nSchedule saved to schedule.txt\n");
+        }
+
+        else if(choice == 3) {
+
+            loadTasks();
+
+            displayTasks();
+        }
+
+        else if(choice == 4) {
+
+            printf("Program Ended.\n");
+            break;
+        }
+
+        else {
+
+            printf("Invalid Choice\n");
         }
     }
+
+    return 0;
 }
